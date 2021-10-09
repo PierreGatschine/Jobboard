cda70264337ca8e37f8b62e98aa36f8a32b2a6b5
PierreGatschine <pierre.gatschine@gmail.com>
2021-10-09 03:47:21 +0200

    ADD components btn wrapper

---
+++ components/btn.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+      <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <NuxtLink to="/form">{{ msg }}</NuxtLink>
+      </button>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    msg: String,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/wrapper.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex justify-center sm:flex-wrap pt-8 sm:pt-0 text-6xl font-bold text-blue-600">toto<span class="text-blue-500">{{ msg }}</span></div>
+
+</template>
+
+<script>
+export default {
+    props:{
+      msg: String,
+    }
+}
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
